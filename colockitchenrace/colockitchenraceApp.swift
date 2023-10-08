0cc82107664b0345e77ae06e1761c798c67f4601
julien.rahier <julien.rahier@persgroep.net>
2023-10-08 10:45:52 +0200

    Initial Commit

---
+++ colockitchenrace/colockitchenraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  colockitchenraceApp.swift
+//  colockitchenrace
+//
+//  Created by Julien Rahier on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct colockitchenraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
