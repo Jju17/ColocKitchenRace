6f94a3e4573d8140367ec104f4dcaeaf66facb7c
julien.rahier <julien.rahier@persgroep.net>
2024-06-19 00:04:20 +0200

    PersistanceKey file at wrong place

---
+++ colockitchenrace/PersistenceKey+Utils.swift
@@ -0,0 +1,27 @@
+//
+//  PersistenceKey+Utils.swift
+//  colockitchenrace
+//
+//  Created by Julien Rahier on 02/06/2024.
+//
+
+import ComposableArchitecture
+import Foundation
+
+extension PersistenceKey where Self == PersistenceKeyDefault<FileStorageKey<User?>> {
+    static var userInfo: Self  {
+        PersistenceKeyDefault(.fileStorage(.userInfo), nil)
+    }
+}
+
+extension PersistenceKey where Self == PersistenceKeyDefault<FileStorageKey<Cohouse?>> {
+    static var cohouse: Self  {
+        PersistenceKeyDefault(.fileStorage(.cohouse), nil)
+    }
+}
+
+extension PersistenceKey where Self == PersistenceKeyDefault<FileStorageKey<News?>> {
+    static var news: Self  {
+        PersistenceKeyDefault(.fileStorage(.news), nil)
+    }
+}
