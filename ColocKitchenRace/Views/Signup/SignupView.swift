bf543076f95b5f2f6a808f1bee23d0e8a4704ad0
Julien R. <julien@rahier.dev>
2026-02-10 18:03:28 +0100

    fix: scrollable signup/signin

--- ColocKitchenRace/Views/Signup/SignupView.swift
+++ ColocKitchenRace/Views/Signup/SignupView.swift
@@ -19,112 +19,114 @@ struct SignupView: View {
     let phoneFieldDelegate = TextFieldDelegate()
 
     var body: some View {
-        VStack(spacing: 10) {
-            Image("logo")
-                .resizable()
-                .frame(width: 150, height: 150, alignment: .center)
-
+        ScrollView {
             VStack(spacing: 10) {
-                HStack(spacing: 15) {
-                    CKRTextField(title: "NAME*", value: $store.signupUserData.firstName)
-                        .textContentType(.givenName)
-                        .textInputAutocapitalization(.words)
-                        .focused(self.$focusedField, equals: .name)
+                Image("logo")
+                    .resizable()
+                    .frame(width: 150, height: 150, alignment: .center)
+
+                VStack(spacing: 10) {
+                    HStack(spacing: 15) {
+                        CKRTextField(title: "NAME*", value: $store.signupUserData.firstName)
+                            .textContentType(.givenName)
+                            .textInputAutocapitalization(.words)
+                            .focused(self.$focusedField, equals: .name)
+                            .submitLabel(.next)
+                            .introspect(.textField, on: .iOS(.v16, .v17)) { textField in
+                                nameFieldDelegate.shouldReturn = {
+                                    self.focusNextField()
+                                    return false
+                                }
+
+                                textField.delegate = nameFieldDelegate
+                            }
+                            .onSubmit { self.focusNextField() }
+                        CKRTextField(title: "SURNAME*", value: $store.signupUserData.lastName)
+                            .textContentType(.familyName)
+                            .textInputAutocapitalization(.words)
+                            .focused(self.$focusedField, equals: .surname)
+                            .submitLabel(.next)
+                            .introspect(.textField, on: .iOS(.v16, .v17)) { textField in
+                                surnameFieldDelegate.shouldReturn = {
+                                    self.focusNextField()
+                                    return false
+                                }
+
+                                textField.delegate = surnameFieldDelegate
+                            }
+                    }
+                    CKRTextField(title: "EMAIL*", value: $store.signupUserData.email)
+                        .textContentType(.emailAddress)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.never)
+                        .focused(self.$focusedField, equals: .email)
                         .submitLabel(.next)
                         .introspect(.textField, on: .iOS(.v16, .v17)) { textField in
-                            nameFieldDelegate.shouldReturn = {
+                            emailFieldDelegate.shouldReturn = {
                                 self.focusNextField()
                                 return false
                             }
 
-                            textField.delegate = nameFieldDelegate
+                            textField.delegate = emailFieldDelegate
                         }
-                        .onSubmit { self.focusNextField() }
-                    CKRTextField(title: "SURNAME*", value: $store.signupUserData.lastName)
-                        .textContentType(.familyName)
-                        .textInputAutocapitalization(.words)
-                        .focused(self.$focusedField, equals: .surname)
+                    CKRTextField(title: "PASSWORD*", value: $store.signupUserData.password, isSecure: true)
+                        .textContentType(.newPassword)
+                        .focused(self.$focusedField, equals: .password)
                         .submitLabel(.next)
                         .introspect(.textField, on: .iOS(.v16, .v17)) { textField in
-                            surnameFieldDelegate.shouldReturn = {
+                            passwordFieldDelegate.shouldReturn = {
                                 self.focusNextField()
                                 return false
                             }
 
-                            textField.delegate = surnameFieldDelegate
+                            textField.delegate = passwordFieldDelegate
                         }
+                    CKRTextField(title: "PHONE", value: $store.signupUserData.phone)
+                        .textContentType(.telephoneNumber)
+                        .focused(self.$focusedField, equals: .phone)
+                        .submitLabel(.done)
+                    VStack(spacing: 12) {
+                        CKRButton("Sign up") {
+                            self.store.send(.signupButtonTapped)
+                        }
+                        .frame(height: 50)
+                        if let errorMessage = store.errorMessage {
+                            Text(errorMessage)
+                                .foregroundStyle(.red)
+                                .font(.footnote)
+                        }
+                        HStack {
+                            Text("Already have an account?")
+                            Button("Click here") {
+                                self.store.send(.goToSigninButtonTapped)
+                            }
+                        }
+                        .font(.system(size: 14))
+                    }
+                    .padding(.top)
                 }
-                CKRTextField(title: "EMAIL*", value: $store.signupUserData.email)
-                    .textContentType(.emailAddress)
-                    .keyboardType(.emailAddress)
-                    .textInputAutocapitalization(.never)
-                    .focused(self.$focusedField, equals: .email)
-                    .submitLabel(.next)
-                    .introspect(.textField, on: .iOS(.v16, .v17)) { textField in
-                        emailFieldDelegate.shouldReturn = {
-                            self.focusNextField()
-                            return false
-                        }
-
-                        textField.delegate = emailFieldDelegate
-                    }
-                CKRTextField(title: "PASSWORD*", value: $store.signupUserData.password, isSecure: true)
-                    .textContentType(.newPassword)
-                    .focused(self.$focusedField, equals: .password)
-                    .submitLabel(.next)
-                    .introspect(.textField, on: .iOS(.v16, .v17)) { textField in
-                        passwordFieldDelegate.shouldReturn = {
-                            self.focusNextField()
-                            return false
-                        }
-
-                        textField.delegate = passwordFieldDelegate
-                    }
-                CKRTextField(title: "PHONE", value: $store.signupUserData.phone)
-                    .textContentType(.telephoneNumber)
-                    .focused(self.$focusedField, equals: .phone)
-                    .submitLabel(.done)
-                VStack(spacing: 12) {
-                    CKRButton("Sign up") {
-                        self.store.send(.signupButtonTapped)
-                    }
-                    .frame(height: 50)
-                    if let errorMessage = store.errorMessage {
-                        Text(errorMessage)
-                            .foregroundStyle(.red)
-                            .font(.footnote)
-                    }
-                    HStack {
-                        Text("Already have an account?")
-                        Button("Click here") {
-                            self.store.send(.goToSigninButtonTapped)
+                .bind($store.focusedField, to: self.$focusedField)
+                .padding(.horizontal)
+                .toolbar {
+                    ToolbarItem(placement: .keyboard) {
+                        Button(action: self.focusPreviousField) {
+                            Image(systemName: "chevron.up")
                         }
                     }
-                    .font(.system(size: 14))
-                }
-                .padding(.top)
-            }
-            .bind($store.focusedField, to: self.$focusedField)
-            .padding(.horizontal)
-            .toolbar {
-                ToolbarItem(placement: .keyboard) {
-                    Button(action: self.focusPreviousField) {
-                        Image(systemName: "chevron.up")
+                    ToolbarItem(placement: .keyboard) {
+                        Button(action: self.focusNextField) {
+                            Image(systemName: "chevron.down")
+                        }
                     }
-                }
-                ToolbarItem(placement: .keyboard) {
-                    Button(action: self.focusNextField) {
-                        Image(systemName: "chevron.down")
+                    ToolbarItem(placement: .keyboard) {
+                        Spacer()
                     }
-                }
-                ToolbarItem(placement: .keyboard) {
-                    Spacer()
                 }
             }
+            .frame(maxWidth: .infinity, minHeight: UIScreen.main.bounds.height)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .scrollDismissesKeyboard(.interactively)
         .background { Color.CKRBlue.ignoresSafeArea() }
-
     }
 }
 
